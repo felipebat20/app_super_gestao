0a96602162f6f95c132d6f1666c11deec58de160
felipebat20 <felipebatista24680@gmail.com>
2021-08-31 07:56:16 -0300

    Entende um pouco mais sobre as tags de impressao de variavel e texto dentro do blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,6 @@
 <h3>Teste de rota</h3>
 
 {{-- Um comentário bem desnecessário no blade. Apenas para informar que é assim que comenta no blade --}}
+{{ '<h3>Teste de texto</h3>' }}
+{!! '<h2>Teste de texto</h2>' !!}
+<?= '<h1>Teste de texto</h1>' ?>
