9b8c74ff1d665b8f3bcbfcbf6ac06d4cfb3c9f36
felipebat20 <felipebatista24680@gmail.com>
2021-08-31 07:28:17 -0300

    Enviando os parametros para view por array associativo

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2) {
-        echo "A soma de $p1 e $p2 é: ".($p1 + $p2);
+        // echo "A soma de $p1 e $p2 é: ".($p1 + $p2);
+        return view('teste', [
+            'x' => $p1,
+            'y' => $p2
+        ]);
     }
 }
---
+++ resources/views/teste.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Mysql > Postgres</title>
+</head>
+<body>
+  <h1>Primeiro parâmetro: {{ $x }}</h1>
+  <p>Segundo parâmetro:  {{ $y }}</p>
+</body>
+</html>
