fa0bcfa1cdc651db79cf896de857766bebb3bbf3
felipebat20 <felipebatista24680@gmail.com>
2021-09-01 06:58:22 -0300

    Entendendo  @if

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,3 +4,13 @@
 {{ '<h3>Teste de texto</h3>' }}
 {!! '<h2>Teste de texto</h2>' !!}
 <?= '<h1>Teste de texto</h1>' ?>
+
+{{-- @dd($fornecedores) --}}
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+  <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores) > 10)
+  <h3>Existem muitos fornecedores cadastrados<h3>
+@else
+  <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
