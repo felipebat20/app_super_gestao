bc341aff4b76f4c20248f9e104c955b7676d4f9c
felipebat20 <felipebatista24680@gmail.com>
2021-09-02 07:40:20 -0300

    Entendendo @forelse

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,23 +1,17 @@
 <h3>Teste de rota</h3>
-
-{{-- Um comentário bem desnecessário no blade. Apenas para informar que é assim que comenta no blade --}}
-{{ '<h3>Teste de texto</h3>' }}
-{!! '<h2>Teste de texto</h2>' !!}
-<?= '<h1>Teste de texto</h1>' ?>
-
-
-{{-- @dd($fornecedores) --}}
 @isset($fornecedores)
   <br>
-  @foreach ($fornecedores as $indice => $fornecedor)
-  Fornecedor: {{ $fornecedor['name'] }}
-  <br/>
-  Status: {{ $fornecedor['status'] }}
-  <br>
-  CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
-  <br>
-  Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] }}
-  <br><br>
-  <hr>
-  @endforeach
+  @forelse ($fornecedores as $indice => $fornecedor)
+    Fornecedor: {{ $fornecedor['name'] }}
+    <br/>
+    Status: {{ $fornecedor['status'] }}
+    <br>
+    CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
+    <br>
+    Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] }}
+    <br><br>
+    <hr>
+  @empty
+    Não existem fornecedores cadastrados.
+  @endforelse
 @endisset
