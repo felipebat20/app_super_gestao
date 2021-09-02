3fdcb62a85256d95e74b540f39b3327a5c7bd1c5
felipebat20 <felipebatista24680@gmail.com>
2021-09-02 07:52:25 -0300

    Entendendo um pouco sobre  das iteraçoes @foreach e @forelse

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,14 +2,24 @@
 @isset($fornecedores)
   <br>
   @forelse ($fornecedores as $indice => $fornecedor)
-    Fornecedor: @{{ $fornecedor['name'] }}
+    Iteração atual: {{ $loop->iteration }}
+    <br>
+    Fornecedor: {{ $fornecedor['name'] }}
     <br/>
-    Status: @{{ $fornecedor['status'] }}
+    Status: {{ $fornecedor['status'] }}
     <br>
-    CNPJ: @{{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
+    CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
     <br>
-    Telefone: @{{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] }}
-    <br><br>
+    Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] }}
+    <br>
+    @if ($loop->first)
+      Essa é a primeira iteração do objeto loop
+    @endif
+    @if ($loop->last)
+      Essa é a ultima iteração do objeto loop
+      <br>
+      Total de registros: {{ $loop->count }}
+    @endif
     <hr>
   @empty
     Não existem fornecedores cadastrados.
