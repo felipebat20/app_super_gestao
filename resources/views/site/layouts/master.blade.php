5973034fe10bf92099ac5932525d09d0a32f4fae
felipebat20 <felipebatista24680@gmail.com>
2021-09-03 23:58:51 -0300

    Adiciona template para reaproveitar o layout com @yield @section @extends

---
+++ resources/views/site/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+
+        <link rel="stylesheet" href="{{ asset('css/estilo_base.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
+    </head>
+
+    <body>
+        @yield('container')
+    </body>
+</html>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,56 +1,48 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_base.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
-    </head>
+@extends('site.layouts.master')
 
-    <body>
-        <div class="topo">
+@section('container')
+    <div class="topo">
 
-            <div class="logo">
-                <img src="{{ asset('img/logo.png') }}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
+        <div class="logo">
+            <img src="{{ asset('img/logo.png') }}">
         </div>
 
-        <div class="conteudo-pagina">
-            <div class="titulo-pagina">
-                <h1>Olá, eu sou o Super Gestão</h1>
-            </div>
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('site.index') }}">Principal</a></li>
+                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+                <li><a href="{{ route('site.contato') }}">Contato</a></li>
+            </ul>
+        </div>
+    </div>
 
-            <div class="informacao-pagina">
-                <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
-                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
-            </div>
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Olá, eu sou o Super Gestão</h1>
         </div>
 
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="{{ asset('img/facebook.png') }}">
-                <img src="{{ asset('img/linkedin.png') }}">
-                <img src="{{ asset('img/youtube.png') }}">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="{{ asset('img/mapa.png') }}">
-            </div>
+        <div class="informacao-pagina">
+            <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+            <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
         </div>
-    </body>
-</html>
+    </div>
+
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('img/facebook.png') }}">
+            <img src="{{ asset('img/linkedin.png') }}">
+            <img src="{{ asset('img/youtube.png') }}">
+        </div>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(11) 3333-4444</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
+        </div>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('img/mapa.png') }}">
+        </div>
+    </div>
+@endsection
