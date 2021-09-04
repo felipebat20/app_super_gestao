4141dbaf873c33da99193b3485aff21435294fb1
felipebat20 <felipebatista24680@gmail.com>
2021-09-04 11:42:04 -0300

    Modela primeira migration site contato

--- database/migrations/2021_09_04_142209_create_site_contatos_table.php
+++ database/migrations/2021_09_04_142209_create_site_contatos_table.php
@@ -15,6 +15,11 @@ class CreateSiteContatosTable extends Migration
     {
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('email', 50);
+            $table->string('telefone', 20 );
+            $table->integer('contact_reason');
+            $table->text('message');
             $table->timestamps();
         });
     }
