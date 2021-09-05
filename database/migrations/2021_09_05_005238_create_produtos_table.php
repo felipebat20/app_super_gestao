50ca65c9ab394811f7b0274253b63619562ecec3
felipebat20 <felipebatista24680@gmail.com>
2021-09-04 22:02:14 -0300

    Entendendo o inicio sobre migrations

---
+++ database/migrations/2021_09_05_005238_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->integer('weight')->nullable();
+            $table->float('sell_price', 8, 2)->default(0.01);
+            $table->integer('min_stack')->default(1);
+            $table->integer('max_stack')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
