3f4cfb5b9e03baad07e78100795b3f5275b3fe1b
felipebat20 <felipebatista24680@gmail.com>
2021-09-06 08:01:57 -0300

    Entendendo sobre Eloquent ORM - ::whereIn and ::whereNotIN

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -22,3 +22,7 @@ class SiteContato extends Model
 }
 //Utilizamos o $fillable para permitir MassAssigment, dessa forma podemos utilizar um método estático ::create para criar registross
 //métodos estáticos não dependem do objeto ser instanciado
+//O método ::where é um construtor é necessário passar para ele um método get para pegar os dados da query
+//$contatos = SiteContato::where('email', 'like', '%@gmail%')->get('name');
+//O método ::where aceita alguns parametros de comparação como >, <, <=, >=, ==, !=, like('%achar em qualquer lugar%')
+// O método ::whereIn('campo', 'parametros', parametros) ::whereNotIn realiza uma comparação por desigualdade
