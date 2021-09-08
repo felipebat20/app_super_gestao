e1ec060f19ec4e48edfdb8fa2a89aceb880e065e
felipebat20 <felipebatista24680@gmail.com>
2021-09-08 07:35:08 -0300

    Entendendo Eloquent ORM - ::whereColumn('coluna', 'comparador', 'outraColuna')

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -30,3 +30,4 @@ class SiteContato extends Model
 // orWhere para comparações ou. Para refinar certa busca podemos utilizar where()->where()->orWhere() encadeando-os.
 // ::whereNull('updated_at') pega todos registros onde updated_at é nulo. O contrário seria ::whereNotNull
 // Tem também ::whereDay('created_at', 31), whereYear('created_at', 2021), whereTime('created_at', '=', '22:57:17')
+// Temos o whereColumn que compara valores de duas colunas diferentes como ::whereColumn('created_at', 'updated_at') retorna os registros cujo os quais a data de criação é igual a data de update.
