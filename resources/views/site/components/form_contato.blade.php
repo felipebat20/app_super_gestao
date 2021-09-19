4538a920910fe6a1b07b9f3c825efae154f5b673
felipebat20 <felipebatista24680@gmail.com>
2021-09-19 08:37:48 -0300

    Customizaçao da apresentaçao dos erros de validaçao pt.2

--- resources/views/site/components/form_contato.blade.php
+++ resources/views/site/components/form_contato.blade.php
@@ -3,10 +3,21 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
     <input name="name" value="{{ old('name') }}" type="text" placeholder="Nome" class="{{ $border }}">
+    @if ($errors->has('name'))
+        <span class="text-danger text-left">
+            {{ $errors->first('name') }}
+        </span>
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $border }}">
+    <span class="text-danger text-left">
+        {{ $errors->first('telefone') ?? '' }}
+    </span>
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $border }}">
+    <span class="text-danger text-left">
+        {{ $errors->first('email') ?? '' }}
+    </span>
     <br>
     <select name="contact_reasons_id" class="{{ $border }}">
         <option value="">Qual o motivo do contato?</option>
@@ -14,15 +25,15 @@
             <option value="{{ $key }}" {{ old('contact_reasons_id') == $key ? 'selected' : '' }}>{{ $motivo }}</option>
         @endforeach
     </select>
+    <span class="text-danger text-left">
+        {{ $errors->first('contact_reasons_id') ?? '' }}
+    </span>
     <br>
     <textarea name="message" value="{{ old('message') }}" class="{{ $border }}">{{ old('message') ?? 'Preencha aqui a sua mensagem' }}
     </textarea>
+    <span class="text-danger text-left">
+        {{ $errors->first('message') ?? '' }}
+    </span>
     <br>
     <button type="submit" class="{{ $border }}">ENVIAR</button>
 </form>
-
-@if ($errors->any())
-    @foreach ($errors->all() as $erro)
-        {{ $erro }} <br>
-    @endforeach
-@endif
--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -4,6 +4,7 @@
         <title>Super Gestão - @yield('title')</title>
         <meta charset="utf-8">
 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
         <link rel="stylesheet" href="{{ asset('css/estilo_base.css')}}">
     </head>
@@ -11,5 +12,7 @@
     <body>
         @include('site.partials.header')
         @yield('container')
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
