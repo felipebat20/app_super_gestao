af105448502d1472b33731d40e807bc705b6b157
felipebat20 <felipebatista24680@gmail.com>
2021-09-19 08:12:52 -0300

    Validaçao de campos unicos com unique

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -29,7 +29,7 @@ class ContatoController extends Controller
         $request->validate([
             'name' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'email',
+            'email' => 'email|unique:site_contatos',
             'contact_reasons_id' => 'required',
             'message' => 'required|max:2000',
         ]);
