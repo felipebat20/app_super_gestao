e186dcf4880f7e257ed1bd58b1a21a3c0fe2b333
felipebat20 <felipebatista24680@gmail.com>
2022-02-12 09:29:03 -0300

    fix: cliente route refered in header

--- resources/views/app/layouts/partials/header.blade.php
+++ resources/views/app/layouts/partials/header.blade.php
@@ -6,7 +6,7 @@
   <div class="menu">
       <ul>
           <li><a href="{{ route('app.home') }}">Home</a></li>
-          <li><a href="{{ route('app.cliente') }}">Cliente</a></li>
+          <li><a href="{{ route('cliente.index') }}">Cliente</a></li>
           <li><a href="{{ route('app.fornecedor') }}">Fornecedor</a></li>
           <li><a href="{{ route('product.index') }}">Produto</a></li>
           <li><a href="{{ route('app.sair') }}">Sair</a></li>
